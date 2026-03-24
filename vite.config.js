e62e1ee3ec34b2a4b4a96ccb94cae5e6844a1fce
dunredd <bruno.tanguy@icloud.com>
2026-03-24 22:19:01 +0100

    Opus 1

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                index:      resolve(__dirname, 'index.html'),
+                resume:     resolve(__dirname, 'resume.html'),
+                decathlon:  resolve(__dirname, 'projectDecathlon.html'),
+                cegedim:    resolve(__dirname, 'projectCegedim.html'),
+                citadelles: resolve(__dirname, 'projectCitadelles.html'),
+                weplan:     resolve(__dirname, 'projectWePlan.html'),
+            }
+        }
+    }
+})
